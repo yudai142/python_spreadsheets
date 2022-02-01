1bb5b23af45919532b569c75ade0462793bae632
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-02-01 17:24:02 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+#jsonファイルを使って認証情報を取得
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+c = ServiceAccountCredentials.from_json_keyfile_name('tenacious-ether-339201-6b7114c9fa99.json', scope)
+
+#認証情報を使ってスプレッドシートの操作権を取得
+gs = gspread.authorize(c)
+
+#共有したスプレッドシートのキー（後述）を使ってシートの情報を取得
+SPREADSHEET_KEY = '1uCzL_evf6AVRtnZr8HVmXHRzzRFAcBqgS53bDGGeTOY'
+worksheet = gs.open_by_key(SPREADSHEET_KEY).worksheet("シート1")
+print(worksheet.acell("B2").value)
